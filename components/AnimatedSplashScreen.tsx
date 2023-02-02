8def5a232941141d4fa6c67d2fef61974cfc5efc
Rahul5430 <rahul213himani@gmail.com>
2023-02-02 23:10:08 +0530

    testing by removing splash screen logo

--- components/AnimatedSplashScreen.tsx
+++ components/AnimatedSplashScreen.tsx
@@ -1,19 +1,20 @@
-import { useEffect, useRef } from 'react';
-import {
-	Animated,
-	Dimensions,
-	Image,
-	StyleSheet,
-	Text,
-	View,
-} from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { Animated, Dimensions, StyleSheet, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+// import * as SplashScreen from 'expo-splash-screen';
 import SplashLogo from '../assets/customSplash.png';
 import Home from './Home';
 
+// // Keep the splash screen visible while we fetch resources
+// SplashScreen.preventAutoHideAsync();
+
 const BGColor = '#F27649';
 
-const SplashScreen = () => {
+const AnimatedSplashScreen = () => {
+	// const [appIsReady, setAppIsReady] = useState(false);
+
+	// console.log(appIsReady);
+
 	// SafeArea Value...
 	const edges = useSafeAreaInsets();
 
@@ -32,6 +33,21 @@ const SplashScreen = () => {
 	const contentTransition = useRef(
 		new Animated.Value(Dimensions.get('window').height)
 	).current;
+
+	// useEffect(() => {
+	// 	const prepare = async () => {
+	// 		try {
+	// 			await new Promise((resolve) => setTimeout(resolve, 2000));
+	// 		} catch (e) {
+	// 			console.warn(e);
+	// 		} finally {
+	// 			// Tell the application to render
+	// 			setAppIsReady(true);
+	// 		}
+	// 	};
+
+	// 	prepare();
+	// }, []);
 
 	// Animation Done....
 	useEffect(() => {
@@ -79,6 +95,21 @@ const SplashScreen = () => {
 			]).start();
 		}, 500);
 	}, []);
+
+	// const onLayoutRootView = useCallback(async () => {
+	// 	if (appIsReady) {
+	// 		// This tells the splash screen to hide immediately! If we call this after
+	// 		// `setAppIsReady`, then we may see a blank screen while the app is
+	// 		// loading its initial state and rendering its first pixels. So instead,
+	// 		// we hide the splash screen once we know the root view has already
+	// 		// performed layout.
+	// 		await SplashScreen.hideAsync();
+	// 	}
+	// }, [appIsReady]);
+
+	// if (!appIsReady) {
+	// 	return null;
+	// }
 
 	return (
 		<View style={styles.outsideView}>
@@ -164,4 +195,4 @@ const styles = StyleSheet.create({
 	},
 });
 
-export default SplashScreen;
+export default AnimatedSplashScreen;
