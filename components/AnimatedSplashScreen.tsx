ecdeeb7475fc49c079752e86402d80f8e7dc44b6
Rahul5430 <rahul213himani@gmail.com>
2023-02-03 23:33:37 +0530

    testing by manually hiding splashscreen

--- components/AnimatedSplashScreen.tsx
+++ components/AnimatedSplashScreen.tsx
@@ -1,19 +1,19 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { Animated, Dimensions, StyleSheet, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-// import * as SplashScreen from 'expo-splash-screen';
+import * as SplashScreen from 'expo-splash-screen';
 import SplashLogo from '../assets/customSplash.png';
 import Home from './Home';
 
-// // Keep the splash screen visible while we fetch resources
-// SplashScreen.preventAutoHideAsync();
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 const BGColor = '#F27649';
 
 const AnimatedSplashScreen = () => {
-	// const [appIsReady, setAppIsReady] = useState(false);
+	const [appIsReady, setAppIsReady] = useState(false);
 
-	// console.log(appIsReady);
+	console.log(appIsReady);
 
 	// SafeArea Value...
 	const edges = useSafeAreaInsets();
@@ -34,20 +34,20 @@ const AnimatedSplashScreen = () => {
 		new Animated.Value(Dimensions.get('window').height)
 	).current;
 
-	// useEffect(() => {
-	// 	const prepare = async () => {
-	// 		try {
-	// 			await new Promise((resolve) => setTimeout(resolve, 2000));
-	// 		} catch (e) {
-	// 			console.warn(e);
-	// 		} finally {
-	// 			// Tell the application to render
-	// 			setAppIsReady(true);
-	// 		}
-	// 	};
+	useEffect(() => {
+		const prepare = async () => {
+			try {
+				await new Promise((resolve) => setTimeout(resolve, 500));
+			} catch (e) {
+				console.warn(e);
+			} finally {
+				// Tell the application to render
+				setAppIsReady(true);
+			}
+		};
 
-	// 	prepare();
-	// }, []);
+		prepare();
+	}, []);
 
 	// Animation Done....
 	useEffect(() => {
@@ -96,23 +96,23 @@ const AnimatedSplashScreen = () => {
 		}, 500);
 	}, []);
 
-	// const onLayoutRootView = useCallback(async () => {
-	// 	if (appIsReady) {
-	// 		// This tells the splash screen to hide immediately! If we call this after
-	// 		// `setAppIsReady`, then we may see a blank screen while the app is
-	// 		// loading its initial state and rendering its first pixels. So instead,
-	// 		// we hide the splash screen once we know the root view has already
-	// 		// performed layout.
-	// 		await SplashScreen.hideAsync();
-	// 	}
-	// }, [appIsReady]);
+	const onLayoutRootView = useCallback(async () => {
+		if (appIsReady) {
+			// This tells the splash screen to hide immediately! If we call this after
+			// `setAppIsReady`, then we may see a blank screen while the app is
+			// loading its initial state and rendering its first pixels. So instead,
+			// we hide the splash screen once we know the root view has already
+			// performed layout.
+			await SplashScreen.hideAsync();
+		}
+	}, [appIsReady]);
 
-	// if (!appIsReady) {
-	// 	return null;
-	// }
+	if (!appIsReady) {
+		return null;
+	}
 
 	return (
-		<View style={styles.outsideView}>
+		<View style={styles.outsideView} onLayout={onLayoutRootView}>
 			<Animated.View
 				style={{
 					...styles.splashView,
