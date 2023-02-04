da3435f84c0fcb7f6bb03faa4b98bcb4ecb268a3
Rahul5430 <rahul213himani@gmail.com>
2023-02-04 23:39:24 +0530

    testing

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,8 +1,9 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, useWindowDimensions, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const Home = () => {
 	const edges = useSafeAreaInsets();
+	const {height, width} = useWindowDimensions();
 
 	return (
 		<View>
@@ -11,6 +12,7 @@ const Home = () => {
 					style={{
 						...styles.outerView,
 						paddingTop: edges.top + 55.5,
+						height: height,
 					}}
 				>
 					<Text>Home</Text>
@@ -24,8 +26,10 @@ const styles = StyleSheet.create({
 	outerView: {
 		paddingLeft: 15,
 		paddingRight: 15,
-		paddingBottom: 1000,
+		// paddingBottom: 1000,
 		backgroundColor: '#FFFFFF',
+		justifyContent: 'center',
+		alignItems: 'center',
 	},
 });
 
